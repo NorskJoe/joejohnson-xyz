4255eca33990b834435606b7147e16c36ae2e793
Joe Johnson <joe.johnson3909@gmail.com>
2025-03-02 09:27:50 +1100

    Add basic styles to footer component

--- apps/resume/src/app/app.tsx
+++ apps/resume/src/app/app.tsx
@@ -1,18 +1,19 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.scss';
+import styles from './app.module.scss';
 
-import { Route, Routes } from "react-router-dom";
-import Projects from "../pages/projects/projects";
-import Experience from "../pages/experience/experience";
-import Home from "../pages/home/home";
+import { Route, Routes } from 'react-router-dom';
+import Projects from '../pages/projects/projects';
+import Experience from '../pages/experience/experience';
+import Home from '../pages/home/home';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/experience" element={<Experience />} />
-    </Routes>
+    <div className={styles['body']}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </div>
   );
 }
 
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,2 +1,2 @@
-export {default as Navbar} from './lib/navbar/navbar';
+export { default as Navbar } from './lib/navbar/navbar';
 export { default as Footer } from './lib/footer/footer';
