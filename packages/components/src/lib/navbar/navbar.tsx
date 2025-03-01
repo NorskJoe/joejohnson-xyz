b1e67248b6b9482127b2c21509be0b83f995e366
Joe Johnson <joe.johnson3909@gmail.com>
2025-03-02 09:07:43 +1100

    Add basic styles to navbar component

--- packages/components/src/lib/navbar/navbar.tsx
+++ packages/components/src/lib/navbar/navbar.tsx
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { NavbarProps } from './navbar.types';
+import styles from './navbar.module.scss';
 
-const Navbar = ({links}: NavbarProps) => {
+const Navbar = ({ links }: NavbarProps) => {
   return (
-    <ul>
+    <ul className={styles['navbar-list']}>
       {links.map((link) => (
-        <li key={link.link}>
+        <li key={link.link} className={styles['navbar-list-item']}>
           <Link to={link.link}>{link.name}</Link>
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
