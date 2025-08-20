66bc48f071e521c23919a1994a77be8501b3837b
NorskJoe <joe.johnson3909@gmail.com>
2025-08-20 14:40:11 +1000

    Add tag stories

--- packages/components/src/lib/tag/tag.stories.tsx
+++ packages/components/src/lib/tag/tag.stories.tsx
@@ -1,13 +1,57 @@
-import type { Meta, StoryObj } from 'storybook/internal/types';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import Tag from './tag';
+import { TagProps } from './tag.types';
 
 const meta: Meta<typeof Tag> = {
   component: Tag,
   title: 'Tag',
+  argTypes: {
+    name: {
+      control: 'text',
+      description: 'The name of the tag to display',
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
+  render: (args: TagProps) => (
+    <div
+      style={{
+        display: 'flex',
+        backgroundColor: '#252525',
+        color: 'white',
+        width: '15rem',
+        padding: '1rem',
+      }}
+    >
+      <Tag {...args} />
+    </div>
+  ),
 };
-export default meta;
-type Story = StoryObj<typeof Tag>;
 
-export const Primary = {
-  args: {},
+export default meta;
+type Story = StoryObj<TagProps>;
+
+export const Small: Story = {
+  args: {
+    name: 'Small Tag',
+    size: 'small',
+  } as TagProps,
+};
+
+export const Medium: Story = {
+  args: {
+    name: 'Medium Tag',
+    size: 'medium',
+  } as TagProps,
+};
+
+export const Large: Story = {
+  args: {
+    name: 'Large Tag',
+    size: 'large',
+  } as TagProps,
 };
