0ef8ce6d9f27ff260c4a293cbc7cd994e9abc234
NorskJoe <joe.johnson3909@gmail.com>
2025-08-20 12:56:39 +1000

    Add image stories

--- packages/components/src/lib/image/image.stories.tsx
+++ packages/components/src/lib/image/image.stories.tsx
@@ -1,13 +1,65 @@
 import type { Meta, StoryObj } from 'storybook/internal/types';
 import Image from './image';
+import { ImageProps } from './image.types';
+import { ImageType } from '../shared/shared.types';
 
 const meta: Meta<typeof Image> = {
   component: Image,
   title: 'Image',
+  argsTypes: {
+    imageUrl: {
+      control: 'text',
+      description: 'URL of the image to display',
+    },
+    imageType: {
+      control: {
+        type: 'select',
+        labels: {
+          [ImageType.ROUND]: 'Round',
+          [ImageType.LOGO]: 'Logo',
+          [ImageType.SQUARE_ZOOM]: 'Square Zoom',
+        },
+      },
+      options: [ImageType.ROUND, ImageType.LOGO, ImageType.SQUARE_ZOOM],
+      description: 'Type of image to display',
+    },
+    altText: {
+      control: 'text',
+      description: 'Alternative text for the image',
+    },
+  },
+  render: (args: ImageProps) => {
+    return (
+      <div style={{margin: '2em'}}>
+        <Image {...args} />
+      </div>
+    );
+  },
 };
-export default meta;
-type Story = StoryObj<typeof Image>;
 
-export const Primary = {
-  args: {},
+export default meta;
+type Story = StoryObj<ImageProps>;
+
+export const Round: Story = {
+  args: {
+    imageUrl: 'https://placehold.jp/3d4070/ffffff/150x150.png',
+    imageType: ImageType.ROUND,
+    altText: 'rounder',
+  } as ImageProps,
+};
+
+export const Square: Story = {
+  args: {
+    imageUrl: 'https://placehold.jp/3d4070/ffffff/150x150.png',
+    imageType: ImageType.SQUARE_ZOOM,
+    altText: 'zoomer',
+  } as ImageProps,
+};
+
+export const Logo: Story = {
+  args: {
+    imageUrl: 'https://placehold.jp/3d4070/ffffff/150x150.png',
+    imageType: ImageType.LOGO,
+    altText: 'logo-er',
+  } as ImageProps,
 };
