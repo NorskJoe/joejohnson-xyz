10577fac73db1c09dde55062a020389a9a561946
Joe Johnson <joe.johnson3909@gmail.com>
2026-05-17 08:18:08 +1000

    Work on styles for recipe detail. Handle no recipes found

--- apps/recipes/src/app/recipes/[title]/page.tsx
+++ apps/recipes/src/app/recipes/[title]/page.tsx
@@ -2,6 +2,8 @@ import { fetchRecipe } from '@actions/get';
 
 import styles from './recipe-detail.module.scss';
 import { measurementTypeToString } from '@libs/utils';
+import { Pencil } from 'lucide-react';
+import Link from 'next/link';
 
 interface RecipeProps {
   // params is next.js dynamic route params
@@ -15,51 +17,72 @@ const RecipeDetailPage = async ({ params }: RecipeProps) => {
 
   return (
     <div className={styles['container']}>
-      <h1 className={styles['recipe-name']}>{recipeName}</h1>
       {recipe ? (
         <>
-          <div className={styles['recipe-info']}>
-            <p>Prep Time: {recipe.prepTimeInMinutes} minutes</p>
-            <p>Cook Time: {recipe.cookTimeInMinutes} minutes</p>
-            <p>Servings: {recipe.servings}</p>
+          <div className={styles['header']}>
+            <div className={styles['top-bar']}>
+              <ul className={styles['tags']}>
+                {recipe.tags.map((tag, index) => (
+                  <li className={styles['tag-item']} key={index}>
+                    {tag.toUpperCase()}
+                  </li>
+                ))}
+              </ul>
+              <div className={styles['edit-button-container']}>
+                <Link
+                  href={`/admin/edit/${title}`}
+                  className={styles['edit-button']}
+                >
+                  <Pencil size={'1rem'} /> <span>Edit Recipe</span>
+                </Link>
+              </div>
+            </div>
+            <div className={styles['recipe-name']}>{recipeName}</div>
+            <div className={styles['recipe-info']}>
+              <div className={styles['recipe-info-item']}>
+                <span>Servings</span>
+                <span>{recipe.servings} People</span>
+              </div>
+              <div className={styles['recipe-info-item']}>
+                <span>Prep Time</span>
+                <span>{recipe.prepTimeInMinutes} mins</span>
+              </div>
+              <div className={styles['recipe-info-item']}>
+                <span>Cook Time</span>
+                <span>{recipe.cookTimeInMinutes} mins</span>
+              </div>
+            </div>
           </div>
-          {recipe.description && (
-            <p className={styles['recipe-description']}>{recipe.description}</p>
-          )}
-          <div className={styles['recipe-details']}>
-            <div className={styles['list']}>
-              <h3>Ingredients</h3>
-              <ul>
+          <div className={styles['recipe-body']}>
+            <div className={styles['left-column']}>
+              <h3 className={styles['section-title']}>Ingredients</h3>
+              <div className={styles['ingredients-list']}>
                 {recipe.recipeIngredients.map((ingredient, index) => (
-                  <div key={index}>
-                    <li>
-                      {ingredient.quantity}
-                      {measurementTypeToString(
-                        ingredient.measurement.type
-                      )}{' '}
-                      {ingredient.ingredient?.name}
-                    </li>
+                  <span className={styles['ingredient-item']} key={index}>
+                    <input type="checkbox" className={styles['checkbox']} />
+                    {ingredient.quantity}{' '}
+                    {measurementTypeToString(ingredient.measurement.type)}{' '}
+                    {ingredient.ingredient?.name}
+                  </span>
+                ))}
+              </div>
+              <div className={styles['quantity-stepper']}>{/* TODO */}</div>
+            </div>
+            <div className={styles['right-column']}>
+              <h3 className={styles['section-title']}>Instructions</h3>
+              <div className={styles['instruction-list']}>
+                {recipe.instructions.map((instruction, index) => (
+                  <div className={styles['instruction-item']} key={index}>
+                    <div className={styles['instruction-step']}>
+                      {index + 1}
+                      <span className={styles['line']}> </span>
+                    </div>
+                    <div className={styles['instruction']}>{instruction}</div>
                   </div>
                 ))}
-              </ul>
-            </div>
-            <div className={styles['list']}>
-              <h3>Instructions</h3>
-              <ul>
-                {recipe.instructions.map((instruction, index) => (
-                  <li key={index}>{instruction}</li>
-                ))}
-              </ul>
+              </div>
             </div>
           </div>
-
-          <ul className={styles['tags']}>
-            {recipe.tags.map((tag, index) => (
-              <li className={styles['tag-item']} key={index}>
-                {tag}
-              </li>
-            ))}
-          </ul>
         </>
       ) : (
         <p>Recipe not found.</p>
--- apps/recipes/src/app/recipes/page.tsx
+++ apps/recipes/src/app/recipes/page.tsx
@@ -24,29 +24,33 @@ const RecipesLandingPage = async () => {
       <h1>Recipes</h1>
       {/* TODO: add pagination and search functionality here */}
       <ul className={styles['recipe-list']}>
-        {data.map((recipe) => (
-          <li key={recipe.id}>
-            <Link
-              href={`/recipes/${titleSlugify(recipe.title)}`}
-              className={styles['recipe-title']}
-            >
-              {recipe.title}
-            </Link>
-            <div className={styles['metadata']}>
-              <span>
-                <Clock className={styles['icon']} />
-                {formatTotalCookTime(
-                  recipe.prepTimeInMinutes,
-                  recipe.cookTimeInMinutes
-                )}
-              </span>
-              <span>
-                <Utensils className={styles['icon']} />
-                Serves {recipe.servings}
-              </span>
-            </div>
-          </li>
-        ))}
+        {data.length > 0 ? (
+          data.map((recipe) => (
+            <li key={recipe.id}>
+              <Link
+                href={`/recipes/${titleSlugify(recipe.title)}`}
+                className={styles['recipe-title']}
+              >
+                {recipe.title}
+              </Link>
+              <div className={styles['metadata']}>
+                <span>
+                  <Clock className={styles['icon']} />
+                  {formatTotalCookTime(
+                    recipe.prepTimeInMinutes,
+                    recipe.cookTimeInMinutes
+                  )}
+                </span>
+                <span>
+                  <Utensils className={styles['icon']} />
+                  Serves {recipe.servings}
+                </span>
+              </div>
+            </li>
+          ))
+        ) : (
+          <h4>No Recipes found</h4>
+        )}
       </ul>
     </div>
   );
