f89abf6271604413fcfb11c7b8ac5ea5f4d992e4
Joe Johnson <joe.johnson3909@gmail.com>
2026-05-22 20:54:21 +1000

    very basic admin styling

--- apps/recipes/src/app/admin/page.tsx
+++ apps/recipes/src/app/admin/page.tsx
@@ -5,7 +5,7 @@ const AdminLandingPage = () => {
   return (
     <div className={styles['container']}>
       <Link href="/admin/add" passHref>
-        <button>Add Recipe</button>
+        <button className={styles['add-button']}>Add Recipe</button>
       </Link>
     </div>
   );
